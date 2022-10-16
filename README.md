ed7b20e949db9fbb3e368bea666fb56087c8f480
Aaron Kaloti <aarons.7007@gmail.com>
2022-10-15 20:07:06 -0700

    Playing around with public key parsing

---
+++ README.md
@@ -0,0 +1,35 @@
+# Golang Keys Playground
+
+The purpose of this repo is for me to play around with the Golang functions that manipulate cryptographic keys.
+
+## Noteworthy Setup Steps (so I don't forget them...)
+
+Generate RSA private key:
+
+``` bash
+openssl genrsa 2048 > keys/rsa_prac.key
+```
+
+Generate RSA public key:
+
+``` bash
+openssl rsa -in keys/rsa_prac.key -pubout > keys/rsa_prac.pub
+```
+
+Generate certificate for RSA key pair:
+(left all fields blank, except set Common Name to FakeCA)
+
+``` bash
+openssl req -new -x509 -nodes -days 365000 -key keys/rsa_prac.key -out keys/rsa_prac.crt
+```
+
+Inspect certificate:
+
+``` bash
+# Certificate details.
+openssl x509 -in keys/rsa_prac.crt -noout -text
+
+# Extract public key from the certificate.
+openssl x509 -pubkey -noout -in keys/rsa_prac.crt
+```
+
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+// Opens public key file and prints out contents.
+func printPubKey(path string) {
+	var keyFileContents []byte
+	keyFileContents, _ = ioutil.ReadFile(path)
+	// fmt.Println("Key file contents")
+	// fmt.Println(keyFileContents)
+
+	// Extract PEM block.
+	// Second return value is file data corresponding to remaining
+	// PEM blocks (if any).
+	var pemBlock *pem.Block
+	pemBlock, _ = pem.Decode(keyFileContents)
+	if pemBlock == nil {
+		fmt.Fprintf(os.Stderr, "Failed to decode PEM block\n")
+		os.Exit(1)
+	}
+	fmt.Printf("Key type: %s\n", pemBlock.Type)
+
+	// Print PEM block in base 64. This produces the same output
+	// as doing `cat` on the key file.
+	fmt.Println("In base64:")
+	var base64Encoding string
+	var pemBlockBytes []byte
+	pemBlockBytes = pemBlock.Bytes
+	base64Encoding = base64.StdEncoding.EncodeToString(pemBlockBytes)
+	fmt.Println(base64Encoding)
+
+	// Parse into object.
+	var pubkey *rsa.PublicKey
+	var err error
+	var tmp any
+	tmp, err = x509.ParsePKIXPublicKey(pemBlockBytes)
+	pubkey = tmp.(*rsa.PublicKey)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to parse into public key object: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Public key object: %s\n", pubkey)
+
+	// Convert public key object back to bytes.
+	pemBlockBytes, err = x509.MarshalPKIXPublicKey(tmp)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to convert public key objecdt back to bytes: %s\n", err)
+		os.Exit(1)
+	}
+	base64Encoding = base64.StdEncoding.EncodeToString(pemBlockBytes)
+	fmt.Println(base64Encoding)
+}
+
+func main() {
+	printPubKey("keys/rsa_prac.pub")
+}
